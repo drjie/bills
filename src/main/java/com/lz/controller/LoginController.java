6dfe5d439b8e2181c5049d14ebe8113546d3f4b5
Administrator <jiexregret@163.com>
2022-05-01 12:06:23 +0800

    小组项目

---
+++ src/main/java/com/lz/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.lz.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    /**
+     * 跳转到登陆页面
+     */
+    @RequestMapping("toLogin")
+    public String toLogin() {
+        return "login";
+    }
+}
