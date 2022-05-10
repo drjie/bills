68f27307791b9a04614f21f3b8fd60dad0f50d30
zhangsan <123456@qq.com>
2022-05-10 19:41:38 +0800

    第一次体交

--- src/main/java/com/lz/Application.java
+++ src/main/java/com/lz/Application.java
@@ -1,8 +1,9 @@
 package com.lz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.lz.dao")
 @SpringBootApplication
 public class Application {
 
